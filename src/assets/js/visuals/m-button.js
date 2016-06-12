8e98179dd3e16a1fb7f19e92a6366bbe7c3eac2c
Braden Marshall <braden1996@hotmail.co.uk>
2016-06-12 19:40:23 +0100

    Improve button module

--- src/assets/js/visuals/m-button.js
+++ src/assets/js/visuals/m-button.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		let target_id = button.attr("data-target");
 		let target = $(target_id);
 
-		if (target.hasClass("m-button__target--active")) {
+		if ((!target.length && button.hasClass("m-button--active")) || target.hasClass("m-button__target--active")) {
 			button.removeClass("m-button--active");
 			target.removeClass("m-button__target--active");
 		} else {
