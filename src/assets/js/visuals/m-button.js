89d7f5362c06c480f0b837992509a440cc73173a
Braden Marshall <braden1996@hotmail.co.uk>
2016-06-15 20:03:45 +0100

    Improve structure of link between HTML and JS

--- src/assets/js/visuals/m-button.js
+++ src/assets/js/visuals/m-button.js
@@ -1,7 +1,7 @@
 $(".m-button").click(function() {
 	let button = $(this);
-	let target_id = button.attr("data-target");
-	let target = $(target_id);
+	let target_id = button.attr("data-button-target");
+	let target = $("[data-button-id='" + target_id + "']");
 
 	if ((!target.length && button.hasClass("m-button--active")) || target.hasClass("m-button__target--active")) {
 		button.removeClass("m-button--active");
