9b23fd2bc1311d0aa9a87b921dc996af9f55039f
Braden Marshall <braden1996@hotmail.co.uk>
2016-07-13 17:07:04 +0100

    Add file I forgot to previously commit

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,10 +8,10 @@ $(document).ready(function() {
 	require("./visuals/m-button.js");
 	require("./visuals/m-tablist.js");
 
-	// Load site
-	require("./site/onclick.js");
+	// Load views
+	require("./views/onclick.js");
 
-	// Load game
-	let game = require("./game/main.js");
+	// Load game app
+	let game = require("./app/main.js");
 	game.ready();
 });
