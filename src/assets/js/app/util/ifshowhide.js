1c22643f05af6dce3f1e9d1144411baa580ee846
Braden Marshall <braden1996@hotmail.co.uk>
2016-07-28 20:15:37 +0100

    Reduce index.html by merging common elements between the grounded and preferred games

--- src/assets/js/app/util/ifshowhide.js
+++ src/assets/js/app/util/ifshowhide.js
@@ -1,13 +1,21 @@
 // This isn't perfect, but it allows us show/hide DOM elements based upon
 // arbitrary externally managed conditions.
+// Modes:
+//	- Strong: element will be shown if ALL conditions are true.
+//	- Weak: element will be shown if ANY condition is true.
 
 let show_values = {};
 
 let not_prefix = "!";
 
 function canShow(ele) {
+	let mode = "strong";
+	if (ele.data("ifshowhide-mode") === "weak") {
+		mode = "weak";
+	}
+
 	for (let data_attribute in show_values) {
-		let value = $(ele).data(data_attribute);
+		let value = ele.data(data_attribute);
 
 		if (value) {
 			let is_not = false;
@@ -17,14 +25,23 @@ function canShow(ele) {
 			}
 
 			if (value in show_values[data_attribute]) {
+				if (ele.hasClass("turd")) {console.log(data_attribute, value, show_values[data_attribute][value], is_not, mode);};
 				if (show_values[data_attribute][value] === is_not) {
-					return false;
+					if (mode === "strong") {
+						return false;
+					}
+				} else {
+					if (mode === "weak") {
+						return true;
+					}
 				}
 			}
 		}
 	}
 
-	return true;
+	if (ele.hasClass("turd")) {console.log("DONE:", show_values, mode === "strong" ? true : false)}
+
+	return mode === "strong" ? true : false;
 }
 
 function ifShowHide(data_attribute, value, show=true) {
